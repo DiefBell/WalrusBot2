5a4cc201cf14265ce6dbdfd8190c7faa9e5548ca
MerrickKing <merrickkainking@gmail.com>
2019-06-20 09:52:29 +0100

    Almost completed the ReactForRole module, just needs to code to move messages completing then some stress-testing.

--- WalrusBot2/Modules/ReactForRoleModule.cs
+++ WalrusBot2/Modules/ReactForRoleModule.cs
@@ -70,7 +70,7 @@ namespace WalrusBot2.Modules
             #region Emote Validity
             Emote emote = null;
             Emoji emoji = null;
-            if(roleEmote[0] == ':') roleEmote = '<' + roleEmote + '>';
+            if (roleEmote[0] == ':') roleEmote = '<' + roleEmote + '>';
             if (!Emote.TryParse(roleEmote, out emote))
             {
                 try
@@ -96,7 +96,7 @@ namespace WalrusBot2.Modules
                 return;
             }
             Embed embed = msg.Embeds.ElementAt(0) as Embed;
-            if(embed.Fields.Length >= 20)
+            if (embed.Fields.Length >= 20)
             {
                 await ReplyAsync("That message already has 20 fields, which is the most that it can support. " +
                     $"Create a new React-for-Role message using `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add`.");
@@ -123,36 +123,146 @@ namespace WalrusBot2.Modules
             #region Confirm Valid Message
             if (msg.Embeds.Count != 1)
             {
-                await ReplyAsync("The message you've supplied doesn't seem to have the right number of embeds, double check you've got the right message ID! " +
-                    $"You may need to create the message first with `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add`.");
+                await ReplyAsync("The message you've supplied doesn't seem to have the right number of embeds, double check you've got the right message ID! ");
                 return;
             }
             Embed embed = msg.Embeds.ElementAt(0) as Embed;
-            if (embed.Fields.Length >= 20)
+            if (embed.Fields.Length < 1)
             {
-                await ReplyAsync("That message already has 20 fields, which is the most that it can support. " +
-                    $"Create a new React-for-Role message using `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add`.");
+                await ReplyAsync("There are no roles in that message yet. " +
+                    $"Try adding some with `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add` first.");
                 return;
             }
             #endregion
             EmbedBuilder builder = new EmbedBuilder();
             EmbedAuthorBuilder author = new EmbedAuthorBuilder().WithName(embed.Author.Value.Name).WithIconUrl(embed.Author.Value.IconUrl);
             builder.WithAuthor(author).WithFooter("React-for-Role").WithDescription(embed.Description);
-            bool deleted = false;
+            EmbedField deletedField = new EmbedField();
             foreach (EmbedField field in embed.Fields)
             {
                 if (field.Name != roleDisplayName) builder.AddField(field.Name, field.Value);
-                else deleted = true;
+                else
+                {
+                    deletedField = field;
+                    break;
+                }
             }
-            if(!deleted)
+            if (deletedField.Name != roleDisplayName)
             {
                 await ReplyAsync($"The role \"{roleDisplayName}\" doesn't even exist in the supplied message. Please check your spelling!");
                 return;
             }
             embed = builder.Build();
             await msg.ModifyAsync(m => m.Embed = embed);
+            // get emoji
+            string emo = deletedField.Value.Split(' ')[0];
+            Emote emote = null;
+            Emoji emoji = null;
+            if (emo[0] == ':') emo = '<' + emo + '>';
+            if (!Emote.TryParse(emo, out emote))
+            {
+                try
+                {
+                    emoji = new Emoji(emo);
+                }
+                catch
+                {
+                    await ReplyAsync("There was an issue with removing the react for that role, so please go do that manually :)");
+                    return;
+                }
+            }
+            IEmote reactEmote;
+            if (emote == null) reactEmote = emoji;
+            else reactEmote = emote;
+            List<IUser> users = await msg.GetReactionUsersAsync(reactEmote, msg.Reactions[reactEmote].ReactionCount).Flatten().ToList();
+            foreach (IUser user in users)
+            {
+                await msg.RemoveReactionAsync(reactEmote, user);
+            }
         }
 
+        [Command("reposition")]
+        [Alias("pos")]
+        public async Task RfrRepositionAsync(ulong msgId, string roleDisplayName, int position)
+            => await RfrRepositionAsync(Context.Channel, msgId, roleDisplayName, position);
+        [Command("reposition")]
+        [Alias("pos")]
+        public async Task RfrRepositionAsync(IMessageChannel channel, ulong msgId, string roleDisplayName, int position)
+        {
+            RestUserMessage msg = await channel.GetMessageAsync(msgId) as RestUserMessage;
+            #region Confirm Valid Message
+            if (msg.Embeds.Count != 1)
+            {
+                await ReplyAsync("The message you've supplied doesn't seem to have the right number of embeds, double check you've got the right message ID! " +
+                    $"You may need to create the message first with `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add`.");
+                return;
+            }
+            Embed embed = msg.Embeds.ElementAt(0) as Embed;
+            if (position > embed.Length)
+            {
+                await ReplyAsync("The position you've specified is greater than the number of roles in the React-for-Role embed! Please try again with a lower number... ");
+                return;
+            }
+            if(embed.Fields == null)
+            {
+                await ReplyAsync("That doesn't appear to be a valid React-for-Role message. Double check you've got the corrent message ID!");
+                return;
+            }
+            if (embed.Footer.Value.ToString() != "React-for-Role")  // throws an exception if the Embed has no footer...
+            {
+                await ReplyAsync("That doesn't appear to be a valid React-for-Role message. Double check you've got the corrent message ID!");
+                return;
+            }
+            #endregion
+            EmbedBuilder builder = new EmbedBuilder();
+            EmbedAuthorBuilder author = new EmbedAuthorBuilder().WithName(embed.Author.Value.Name).WithIconUrl(embed.Author.Value.IconUrl);
+            builder.WithAuthor(author).WithFooter("React-for-Role").WithDescription(embed.Description);
+            EmbedField moveField = embed.Fields.Where(x => x.Name == roleDisplayName).First();
+            int i = 1;
+            foreach (EmbedField field in embed.Fields)
+            {
+                if (position == i++) builder.AddField(moveField.Name, moveField.Value);
+                if (field.Name != roleDisplayName) builder.AddField(field.Name, field.Value);
+            }
+            embed = builder.Build();
+            await msg.ModifyAsync(m => m.Embed = embed);
+        }
 
+        [Command("move")]
+        [Alias("mv")]
+        public async Task RfrMoveAsync(IMessageChannel fromChannel, ulong msgId, bool delOld=true)
+            => await RfrMoveAsync(fromChannel, msgId, Context.Channel as IMessageChannel, delOld);
+        [Command("move")]
+        [Alias("mv")]
+        public async Task RfrMoveAsync(ulong msgId, IMessageChannel toChannel, bool delOld=true)
+            => await RfrMoveAsync(Context.Channel, msgId, toChannel, delOld);
+        [Command("move")]
+        [Alias("mv")]
+        public async Task RfrMoveAsync(IMessageChannel fromChannel, ulong msgId, IMessageChannel toChannel, bool delOld=true)
+        {
+            RestUserMessage oldMsg = await fromChannel.GetMessageAsync(msgId) as RestUserMessage;
+            #region Confirm Valid Message
+            if (oldMsg.Embeds.Count != 1)
+            {
+                await ReplyAsync("The message you've supplied doesn't seem to have the right number of embeds, double check you've got the right message ID! " +
+                    $"You may need to create the message first with `{database["config", Program.Debug ? "botDebugPrefix" : "botPrefix"]}rfr add`.");
+                return;
+            }
+            Embed embed = oldMsg.Embeds.ElementAt(0) as Embed;
+            if (embed.Fields == null)
+            {
+                await ReplyAsync("That doesn't appear to be a valid React-for-Role message. Double check you've got the corrent message ID!");
+                return;
+            }
+            if (embed.Footer.Value.ToString() != "React-for-Role")  // throws an exception if the Embed has no footer...
+            {
+                await ReplyAsync("That doesn't appear to be a valid React-for-Role message. Double check you've got the corrent message ID!");
+                return;
+            }
+            #endregion
+            var newMsg = await toChannel.SendMessageAsync("", false, embed);
+            List<IEmote> emotes = oldMsg.Reactions.Keys.ToList();
+            foreach
+        }
     }
 }
